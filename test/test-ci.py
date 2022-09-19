5da8c7f813f7af89031fc8954a1c314f63475877
Cristóbal Sabella Rosa <71850264+CursedSalame@users.noreply.github.com>
2022-09-18 22:02:59 -0300

    INDUSTRIAL SOCIETY AND ITS FUTURE  Introduction  1. The Industrial Revolution and its consequences have been a disaster for the human race. They have greatly increased the life-expectancy of those of us who live in “advanced” countries, but they have destabilized society, have made life unfulfilling, have subjected human beings to indignities, have led to widespread psychological suffering (in the Third World to physical suffering as well) and have inflicted severe damage on the natural world. The continued development of technology will worsen the situation. It will certainly subject human beings to greater indignities and inflict greater damage on the natural world, it will probably lead to greater social disruption and psychological suffering, and it may lead to increased physical suffering even in “advanced” countries.  2. The industrial-technological system may survive or it may break down. If it survives, it MAY eventually achieve a low level of physical and psychological suffering, but only after passing through a long and very painful period of adjustment and only at the cost of permanently reducing human beings and many other living organisms to engineered products and mere cogs in the social machine. Furthermore, if the system survives, the consequences will be inevitable: There is no way of reforming or modifying the system so as to prevent it from depriving people of dignity and autonomy.  3. If the system breaks down the consequences will still be very painful. But the bigger the system grows the more disastrous the results of its breakdown will be, so if it is to break down it had best break down sooner rather than later.

--- test/test-ci.py
+++ test/test-ci.py
@@ -5,9 +5,9 @@ from main import app
 
 client = TestClient(app)
 
-def test():
-	response = client.post("/users/signin", json={"email" : "pepito@pepe.com", "password": "pepe"})
-	assert response.status_code == 200
-	assert response.json() == "Todo OK"
+def test_user_cant_login_if_it_doesnt_exist():
+	response = client.post("/users/signin", json={"email" : "esteUsuario@NoExiste.com", "password": "MatenmePorFavor"})
+	assert response.status_code == 403
+	assert response.json() == {"detail": "Incorrect mail or password"}
 	
 test()
