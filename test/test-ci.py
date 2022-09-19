d89d99e420788c953fe8059d233abf4e36fd5dd1
Cristóbal Sabella Rosa <71850264+CursedSalame@users.noreply.github.com>
2022-09-18 21:54:59 -0300

    Juani, si no funciona sos pollo

--- test/test-ci.py
+++ test/test-ci.py
@@ -1,11 +1,11 @@
 from fastapi.testclient import TestClient
-import sys
-sys.path.append('../app')
-from main import app
+from app.main import app
 
 client = TestClient(app)
 
 def test():
-	assert(True)
-
+	response = client.post("/users/signin", json={"email" : "pepito@pepe.com", "password": "pepe"})
+	assert response.status_code == 200
+	assert response.json() == "Todo OK"
+	
 test()
