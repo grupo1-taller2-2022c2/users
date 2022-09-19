d87d60c4154f23ba40d8019c9a658d06a425f605
Cristóbal Sabella Rosa <71850264+CursedSalame@users.noreply.github.com>
2022-09-18 22:06:53 -0300

    9. The two psychological tendencies that underlie modern leftism we call “feelings of inferiority” and “oversocialization.” Feelings of inferiority are characteristic of modern leftism as a whole, while oversocialization is characteristic only of a certain segment of modern leftism; but this segment is highly influential.  FEELINGS OF INFERIORITY  10. By “feelings of inferiority” we mean not only inferiority feelings in the strict sense but a whole spectrum of related traits; low self-esteem, feelings of powerlessness, depressive tendencies, defeatism, guilt, self- hatred, etc. We argue that modern leftists tend to have some such feelings (possibly more or less repressed) and that these feelings are decisive in determining the direction of modern leftism.  11. When someone interprets as derogatory almost anything that is said about him (or about groups with whom he identifies) we conclude that he has inferiority feelings or low self-esteem. This tendency is pronounced among minority rights activists, whether or not they belong to the minority groups whose rights they defend. They are hypersensitive about the words used to designate minorities and about anything that is said concerning minorities. The terms “negro,” “oriental,” “handicapped” or “chick” for an African, an Asian, a disabled person or a woman originally had no derogatory connotation. “Broad” and “chick” were merely the feminine equivalents of “guy,” “dude” or “fellow.” The negative connotations have been attached to these terms by the activists themselves. Some animal rights activists have gone so far as to reject the word “pet” and insist on its replacement by “animal companion.” Leftish anthropologists go to great lengths to avoid saying anything about primitive peoples that could conceivably be interpreted as negative. They want to replace the world “primitive” by “nonliterate.” They seem almost paranoid about anything that might suggest that any primitive culture is inferior to our own. (We do not mean to imply that primitive cultures ARE inferior to ours. We merely point out the hypersensitivity of leftish anthropologists.)

--- test/test-ci.py
+++ test/test-ci.py
@@ -7,6 +7,8 @@ client = TestClient(app)
 
 def test_user_cant_login_if_it_doesnt_exist():
 	response = client.post("/users/signin", json={"email" : "esteUsuario@NoExiste.com", "password": "MatenmePorFavor"})
+	print(response.status_code)
+	print(response.json())
 	assert response.status_code == 403
 	assert response.json() == {"detail": "Incorrect mail or password"}
 	
