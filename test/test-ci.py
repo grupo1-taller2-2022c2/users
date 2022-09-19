9c4cf332066eb9069ad2918eccf0f519927fd8e7
Cristóbal Sabella Rosa <71850264+CursedSalame@users.noreply.github.com>
2022-09-18 22:08:37 -0300

    12. Those who are most sensitive about “politically incorrect” terminology are not the average black ghetto- dweller, Asian immigrant, abused woman or disabled person, but a minority of activists, many of whom do not even belong to any “oppressed” group but come from privileged strata of society. Political correctness has its stronghold among university professors, who have secure employment with comfortable salaries, and the majority of whom are heterosexual white males from middle- to upper-middle-class families.  13. Many leftists have an intense identification with the problems of groups that have an image of being weak (women), defeated (American Indians), repellent (homosexuals) or otherwise inferior. The leftists themselves feel that these groups are inferior. They would never admit to themselves that they have such feelings, but it is precisely because they do see these groups as inferior that they identify with their problems. (We do not mean to suggest that women, Indians, etc. ARE inferior; we are only making a point about leftist psychology.)  14. Feminists are desperately anxious to prove that women are as strong and as capable as men. Clearly they are nagged by a fear that women may NOT be as strong and as capable as men.  15. Leftists tend to hate anything that has an image of being strong, good and successful. They hate America, they hate Western civilization, they hate white males, they hate rationality. The reasons that leftists give for hating the West, etc. clearly do not correspond with their real motives. They SAY they hate the West because it is warlike, imperialistic, sexist, ethnocentric and so forth, but where these same faults appear in socialist countries or in primitive cultures, the leftist finds excuses for them, or at best he GRUDGINGLY admits that they exist; whereas he ENTHUSIASTICALLY points out (and often greatly exaggerates) these faults where they appear in Western civilization. Thus it is clear that these faults are not the leftist’s real motive for hating America and the West. He hates America and the West because they are strong and successful.  16. Words like “self-confidence,” “self-reliance,” “initiative,” “enterprise,” “optimism,” etc., play little role in the liberal and leftist vocabulary. The leftist is anti-individualistic, pro-collectivist. He wants society to solve everyone’s problems for them, satisfy everyone’s needs for them, take care of them. He is not the sort of person who has an inner sense of confidence in his ability to solve his own problems and satisfy his own needs. The leftist is antagonistic to the concept of competition because, deep inside, he feels like a loser.

--- test/test-ci.py
+++ test/test-ci.py
@@ -6,7 +6,7 @@ from main import app
 client = TestClient(app)
 
 def test_user_cant_login_if_it_doesnt_exist():
-	response = client.post("/users/signin", json={"email" : "esteUsuario@NoExiste.com", "password": "MatenmePorFavor"})
+	response = client.post("/signin", json={"email" : "esteUsuario@NoExiste.com", "password": "MatenmePorFavor"})
 	print(response.status_code)
 	print(response.json())
 	assert response.status_code == 403
